c5bc8d0595e036d8dd745082fc2e96629371a08f
Devesh Gupta <deveshgupta694@gmail.com>
2026-01-31 17:41:36 +0530

    L3 created a component

---
+++ vite-project/src/chai.jsx
@@ -0,0 +1,12 @@
+
+
+function Chai(){ // function name should start with capital letter
+    return (
+        <>
+            <h1>ehehehheh</h1>
+            <p>Hello</p>
+        </>
+    )
+}
+
+export default Chai
